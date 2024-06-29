d6dc5536bb81191899fe46a892e9011dd904dffd
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-29 15:12:34 +0700

    Add transaction client component

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,22 +1,24 @@
 "use client";
-
+import { useRef } from "react";
 import addTransaction from "@/app/actions/addTransaction";
 import { toast } from "react-toastify";
 
 const AddTransaction = () => {
+  const formRef = useRef<HTMLFormElement>(null);
   const clientAction = async (formData: FormData) => {
     const { data, error } = await addTransaction(formData);
 
     if (error) {
       toast.error(error);
     } else {
-      console.log(data);
+      toast.success("Transaction added");
+      formRef.current?.reset();
     }
   };
   return (
     <>
       <h3>Add Transaction</h3>
-      <form action={clientAction}>
+      <form ref={formRef} action={clientAction}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
