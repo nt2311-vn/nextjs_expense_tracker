98b1c8a9fdfe8a3adb0cd3f5b9d00b09e035d96d
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-29 12:40:16 +0700

    use addTransaction function to components

---
+++ components/AddTransaction.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import addTransaction from "@/app/actions/addTransaction";
+
+const AddTransaction = () => {
+  const clientAction = async (formData: FormData) => {
+    const { data, error } = await addTransaction(formData);
+
+    if (error) {
+      alert(error);
+    } else {
+      alert("Transaction added successfully");
+    }
+  };
+  return (
+    <>
+      <h3>Add Transaction</h3>
+      <form action={clientAction}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            id="text"
+            name="text"
+            placeholder="Enter your expense description"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br /> (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            placeholder="Enter amount..."
+            step="0.01"
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransaction;
