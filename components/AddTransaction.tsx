7886357ea032daf40dfab152732947ddae2b3a48
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-29 12:48:22 +0700

    add Trasaction client component

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import addTransaction from "@/app/actions/addTransaction";
+import { toast } from "react-toastify";
 
 const AddTransaction = () => {
   const clientAction = async (formData: FormData) => {
     const { data, error } = await addTransaction(formData);
 
     if (error) {
-      alert(error);
+      toast.error(error);
     } else {
-      alert("Transaction added successfully");
+      console.log(data);
     }
   };
   return (
