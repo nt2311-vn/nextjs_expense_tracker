cdc4cd14351f7cadbfe1451f4da7d6c28ff2a39d
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-30 17:23:27 +0700

    add actions to query transactions

---
+++ app/actions/getTransactions.ts
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+import { Transaction } from "@/types/Transaction";
+
+const getTransactions = async (): Promise<{
+  transactions?: Transaction[];
+  error?: string;
+}> => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return { error: "User not found" };
+  }
+
+  try {
+    const transactions = await db.transaction.findMany({
+      where: { userId },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return { transactions };
+  } catch (err) {
+    return { error: "Database error" };
+  }
+};
+
+export default getTransactions;
